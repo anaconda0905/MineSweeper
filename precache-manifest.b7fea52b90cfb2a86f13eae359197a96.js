c0086fff7186f67a13af85734d73a39ad3caecf6
金龙 <conda0905@gmail.com>
2020-10-19 09:40:32 +0800

    Product version

---
+++ precache-manifest.b7fea52b90cfb2a86f13eae359197a96.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "89b5b4fb91dd8bbb5d050ee669da228b",
+    "url": "/static/media/mark_flag.89b5b4fb.svg"
+  },
+  {
+    "revision": "5214be24780a84429ce0258588024ba3",
+    "url": "/static/media/mark_bomb.5214be24.svg"
+  },
+  {
+    "revision": "6e90c0b2b37d7782dc39b331f46bfef6",
+    "url": "/static/media/bomb.6e90c0b2.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1e816a6fb65020e0ce8e",
+    "url": "/static/js/main.1e816a6f.chunk.js"
+  },
+  {
+    "revision": "e65bac3b8c1379d399f1",
+    "url": "/static/js/1.e65bac3b.chunk.js"
+  },
+  {
+    "revision": "1e816a6fb65020e0ce8e",
+    "url": "/static/css/main.30787f16.chunk.css"
+  },
+  {
+    "revision": "8af95ae7e5937a878746e156424d2592",
+    "url": "/index.html"
+  }
+];
